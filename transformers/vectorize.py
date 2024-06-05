4b4365561a88998c3230d8bf58ed8afc5b01bc72
Alexander Kolmakov <alexander.kolmakov@hwschool.online>
2024-06-05 19:37:32 +0400

    Updated

--- transformers/vectorize.py
+++ transformers/vectorize.py
@@ -8,37 +8,42 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-mlflow.set_experiment("random-forest-hyperopt")
-mlflow.sklearn.autolog()
+
 mlflow.set_tracking_uri("http://mlflow:8012")
 
 
 
 @transformer
 def transform(data, *args, **kwargs):
-    with mlflow.start_run():
-
-        categorical = ['PULocationID', 'DOLocationID']
-        numerical = ['trip_distance']
-
-        dv = DictVectorizer()
-
-        train_dicts = data[categorical + numerical].to_dict(orient='records')
-        X_train = dv.fit_transform(train_dicts)
-        print('vectorizer made')
-
-        target = 'duration'
-        y_train = data[target].values
+    client = mlflow.MlflowClient()
+    run = client.create_run(2)
+    run = mlflow.start_run(run_id = run.info.run_id)
 
 
-        lr = LinearRegression()
-        lr.fit(X_train, y_train)
-        print('model trained')
-        y_train_pred = lr.predict(X_train)
-        rmse_train = mean_squared_error(y_train, y_train_pred, squared=False)
+    categorical = ['PULocationID', 'DOLocationID']
+    numerical = ['trip_distance']
 
-        mlflow.log_metric("rmse", rmse_train)
-        mlflow.log_metric("intercept", lr.intercept_)
+    dv = DictVectorizer()
+
+    train_dicts = data[categorical + numerical].to_dict(orient='records')
+    X_train = dv.fit_transform(train_dicts)
+    print('vectorizer made')
+
+    target = 'duration'
+    y_train = data[target].values
+
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    print('model trained')
+    y_train_pred = lr.predict(X_train)
+    rmse_train = mean_squared_error(y_train, y_train_pred, squared=False)
+
+    mlflow.log_metric("rmse", rmse_train)
+    mlflow.log_metric("intercept", lr.intercept_)
+    mlflow.sklearn.log_model(lr, "model")
+    mlflow.sklearn.log_model(dv, "vectorizer")
+    mlflow.end_run()
 
     return dv, lr
 
